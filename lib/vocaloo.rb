ce99a03729b27d2e59e4feb0f193688f08e26f41
Donn Pebe <iam@donnpebe.com>
2014-04-25 14:20:53 +0700

    add basic features

--- lib/vocaloo.rb
+++ lib/vocaloo.rb
@@ -1,5 +1,73 @@
 require "vocaloo/version"
 
 module Vocaloo
-  # Your code goes here...
+  module InstanceMethods
+    def hyperbolize(opts={})
+      Vocaloo.hyperbolize(self, opts)
+    end
+
+    def dramatize(opts={})
+      Vocaloo.dramatize(self, opts)
+    end
+
+    def stringosaur
+      Vocaloo.stringosaur(self)
+    end
+
+    def upcase?
+      !!self.match(/\p{Upper}/)
+    end
+
+    def downcase?
+      !!self.match(/\p{Lower}/)
+    end
+  end
+
+  def self.vowels
+    ['a','i','u','e','o']
+  end
+
+  def self.hyperbolize(str, opts={})
+    length = (opts[:length] ||= 3)  
+    new_chunk = []
+    str.split('').each do |a|
+      if vowels.include? a.downcase 
+        new_chunk << a*length
+      else
+        new_chunk << a
+      end
+    end
+    new_chunk.join
+  end
+
+  def self.dramatize(str, opts={})
+    length = (opts[:length] ||= 3)
+    return "#{str}#{'!'*length}"
+  end
+
+  def self.stringosaur(str)
+    new_chunk = []
+    find_vocal = []
+    str.split('').each do |a|
+      if vowels.include? a.downcase 
+        find_vocal << a
+      else
+        if find_vocal.empty?
+          new_chunk << a
+        else
+          patch = (find_vocal.last.upcase?) ? "wr".upcase : "wr"
+          new_chunk << "#{find_vocal.join}#{patch}#{a}"
+          find_vocal = []
+        end
+      end
+    end
+    unless find_vocal.empty?
+      patch = (find_vocal.last.upcase?) ? "wr".upcase : "wr"
+      new_chunk << "#{find_vocal.join}#{patch}"
+    end
+
+    new_chunk.join
+  end
 end
+
+String.send :include, Vocaloo::InstanceMethods
--- spec/vocaloo_spec.rb
+++ spec/vocaloo_spec.rb
@@ -1,7 +1,77 @@
 require 'spec_helper'
 
-describe Vocaloo do
-  it 'does stuff' do
-    pending 
+describe String do
+  let(:plain_string) { "GOal!" }
+  let(:hyperbolize_string) { "GOOOaaal!" }
+  let(:a4_hyperbolize_string) { "GOOOOaaaal!" }
+  let(:dramatize_string) { "GOal!!!!" }
+  let(:a4_dramatize_string) { "GOal!!!!!" } 
+  let(:stringosaur_string) { "GOawrl!" }
+  let(:duplicate_vowels) { "GOOOGLE" }
+  let(:stringosaur_duplicate_vowels) { "GOOOWRGLEWR" } 
+
+  describe '::hyperbolize' do
+    it 'should return a string with double vowels then before' do
+      subject.hyperbolize(plain_string).should == hyperbolize_string
+    end
+  end
+
+  describe '::hyperbolize(plain_string, { :length => 4 }' do
+    it 'should return a string with quadruple vowels then before' do
+      subject.hyperbolize(plain_string, { :length => 4 }).should == a4_hyperbolize_string
+    end
+  end
+
+  describe '::dramatize' do
+    it "should return a string that adds with triple '!'" do
+      subject.dramatize(plain_string).should == dramatize_string
+    end
+  end
+
+  describe '::dramatize(plain_string, { :length => 4 }' do
+    it "should return a string that adds with quadruple '!'" do
+      subject.dramatize(plain_string, { :length => 4 }).should == a4_dramatize_string
+    end
+  end
+
+  describe '::stringosaur' do
+    it "should return a string that adds dinosaurs lingo" do
+      subject.stringosaur(plain_string).should == stringosaur_string
+    end
+  end
+
+  describe '::stringosaur' do
+    it "should only apply the transformation in the last sequence vocals" do
+      duplicate_vowels.stringosaur.should == stringosaur_duplicate_vowels
+    end
+  end
+
+  describe '.In String instance' do
+    describe '#hyperbolize' do
+      it 'should respond to hyperbolize' do
+        plain_string.should be_respond_to(:hyperbolize)
+      end
+      it 'should return hyperbolize string' do
+        plain_string.hyperbolize.should == hyperbolize_string
+      end
+    end
+
+    describe '#dramatize' do
+      it 'should respond to dramatize' do
+        plain_string.should be_respond_to(:dramatize)
+      end
+      it 'should return dramatize string' do
+        plain_string.dramatize.should == dramatize_string
+      end
+    end
+
+    describe '#stringosaur' do
+      it 'should respond to stringosaur' do
+        plain_string.should be_respond_to(:stringosaur)
+      end
+      it 'should return stringosaur string' do
+        plain_string.stringosaur.should == stringosaur_string
+      end
+    end
   end
 end
